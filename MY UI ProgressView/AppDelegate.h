81bf5628b4c53e4921710b7409b7d8653a3d74f5
Markandayan Perumandi <marklevy1518@gmail.com>
2018-10-17 12:18:14 +0530

    Initial Commit

---
+++ MY UI ProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MY UI ProgressView
+//
+//  Created by Markandayan Perumandi on 17/10/18.
+//  Copyright © 2018 Markandayan Perumandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MY UI ProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MY UI ProgressView
+//
+//  Created by Markandayan Perumandi on 17/10/18.
+//  Copyright © 2018 Markandayan Perumandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
